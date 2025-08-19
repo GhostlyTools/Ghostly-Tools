fe7be5e100a60f76ab7b5898a71131f9ec2fa7fc
GhostlyTools <Jesse.bartosch@gmail.com>
2025-08-19 02:49:36 -0500

    Update save_users.php

--- save_users.php
+++ save_users.php
@@ -1,18 +1,9 @@
 <?php
-// save_users.php
-header("Content-Type: application/json");
-
-// Get incoming JSON from POST
 $data = file_get_contents("php://input");
-
-// Validate JSON
-if (json_decode($data) === null) {
-    echo json_encode(["status" => "error", "message" => "Invalid JSON"]);
-    exit;
+if ($data) {
+    file_put_contents("users.json", $data);
+    echo "Saved";
+} else {
+    echo "No data received";
 }
-
-// Save to users.json
-file_put_contents("users.json", $data);
-
-echo json_encode(["status" => "ok"]);
 ?>
